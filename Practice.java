1fcd7d1b0059f1433dab5f37cc10439becefb470
nathanso0 <nathan.so1165@gmail.com>
2024-05-09 13:07:42 -0400

    practice1

--- Practice.java
+++ Practice.java
@@ -6,6 +6,24 @@ class Practice extends ConsoleProgram {
   */
 
   public void run() {
+    // Initialize player and scores arrays
+    String[] team = {"James", "Bobby",  "Charles",  "Tommy",  "Fredie"};
+    int[] score = {42, 61, 36, 41, 20};
+    // printing team names
+    for (int i = 0; i < team.length; i++) {
+      System.out.println(team[i] + ": " + score[i]);
+    }
+
+    // Variablees p2
+    int currentindex = 0;
+    int currentmax = score[0];
+    for (int i = 1; i < team.length; i++) {
+      if (score[i] > currentmax) {
+        currentindex = i;
+        currentmax = score[i];
+      }
+    }
+    System.out.println("The max score is: " + currentmax + " and the player who is at max value is: " + team[currentindex]);
 
   }
 }
